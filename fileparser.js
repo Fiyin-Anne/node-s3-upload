396815e7c7608d55dac08b7aaa31bffea7555a03
fiyin-anne <fiyinakinsiku@gmail.com>
2023-04-08 12:19:09 +0100

    upload to s3

---
+++ fileparser.js
@@ -0,0 +1,90 @@
+const formidable = require('formidable');
+const { Upload } = require("@aws-sdk/lib-storage");
+const { S3Client, S3 } = require("@aws-sdk/client-s3");
+const Transform = require('stream').Transform;
+
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.S3_REGION;
+const Bucket = process.env.S3_BUCKET;
+
+const parsefile = async (req) => {
+    return new Promise((resolve, reject) => {
+        let options = {
+            maxFileSize: 100 * 1024 * 1024, //100 megabytes converted to bytes,
+            allowEmptyFiles: false
+        }
+
+        const form = formidable(options);
+        // method accepts the request and a callback.
+        form.parse(req, (err, fields, files) => {
+            // console.log(fields, "====", files)
+        });
+
+        form.on('error', error => {
+            reject(error.message)
+        })
+
+        form.on('data', data => {
+            if (data.name === "complete") {
+                let statuscode = data.value['$metadata']?.httpStatusCode || 200;
+                resolve({
+                    message: "Successfully uploaded!",
+                    data: data.value
+                })
+            }
+        })
+
+        form.on('fileBegin', (formName, file) => {
+
+            file.open = async function () {
+                this._writeStream = new Transform({
+                    transform(chunk, encoding, callback) {
+                        callback(null, chunk)
+                    }
+                })
+
+                this._writeStream.on('error', e => {
+                    form.emit('error', e)
+                });
+
+                // upload to S3
+                new Upload({
+                    client: new S3Client({
+                        credentials: {
+                            accessKeyId,
+                            secretAccessKey
+                        },
+                        region
+                    }),
+                    params: {
+                        ACL: 'public-read',
+                        Bucket,
+                        Key: `${Date.now().toString()}-${this.originalFilename}`,
+                        Body: this._writeStream
+                    },
+                    tags: [], // optional tags
+                    queueSize: 4, // optional concurrency configuration
+                    partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+                    leavePartsOnError: false, // optional manually handle dropped parts
+                })
+                    .done()
+                    .then(data => {
+                        form.emit('data', { name: "complete", value: data });
+                    }).catch((err) => {
+                        form.emit('error', err);
+                    })
+            }
+
+            file.end = function (cb) {
+                this._writeStream.on('finish', () => {
+                    this.emit('end')
+                    cb()
+                })
+                this._writeStream.end()
+            }
+        })
+    })
+}
+
+module.exports = parsefile;
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.set('json spaces', 5);
+
+const PORT = process.env.PORT;
+const fileparser = require('./fileparser');
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h2>File Upload With <code>"Node.js"</code></h2>
+    <form action="/api/upload" enctype="multipart/form-data" method="post">
+      <div>Select a file: 
+        <input name="file" type="file" />
+      </div>
+      <input type="submit" value="Upload" />
+    </form>
+
+  `);
+});
+
+app.post('/api/upload', async (req, res) => {
+  await fileparser(req)
+  .then(data => {
+    res.status(200).json({
+      message: "Success",
+      data
+    })
+  })
+  .catch(error => {
+    res.status(400).json({
+      message: "An error occurred.",
+      error
+    })
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+})
